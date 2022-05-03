70871943c93048ac650af3a6a5eb4e6d74539734
jonbergq <jonbergq@stud.ntnu.no>
2022-05-03 19:40:04 +0200

    updated main files

--- graphinh.py
+++ graphinh.py
@@ -4,20 +4,26 @@ import pandas as pd
 import numpy as numpy
 
 #Key Variables
-powerConsumption = 300.4 #per second, we're working as if it was every 10th minute #actually 201.268+27.42
-usedPower = 201.268+27.42
+powerConsumption = 300.4 #per second, we're working as if it was every 10th minute #actually 201.268+27.72
+usedPower = 201.268+27.72
 powerConsumption = usedPower
-bufferLength = 24*50 #in hours
-bufferCapacity = 39571.2 * bufferLength/16 #total MW capacity of buffer. 23742720 MW for 18 hours 39571.2 divided by 10 minutes. data is for installed capacity, not utilized
+bufferLength = 24*0 +0 #in hours 24*48 + 9 4*33 + 15 for new 45 windmills 24*4 + 7 25: 3 + 18
+bufferCapacity = 1373.928 * bufferLength #total MW capacity of buffer. 23742720 MW for 18 hours 39571.2 divided by 10 minutes. data is for installed capacity, not utilized
+#14818982.4 for 18 hours with actual usedPower, which is 823276.8 for 1 hours. for this task 24698.304 for 18, 1372.128 for 1 hours
 bufferFillingEfficiency = 0.5 #how many % of millpower get turned to buffer, seems to be about 50% for ammonia with electrolysis
-bufferUsingEfficiency = 0.6 #how much energy do you need to use in the buffer to gain enough in the fill
+bufferUsingEfficiency = 0.6 #how much energy do you need to use in the buffer to gain enough in the fill 0.6
 bufferCurrentSize = bufferCapacity #
-windmillAmount = 33 #33 from excel, 16 for basically covering the minimum (above), 41 for covering the numbers today rounded up for all platforms
+windmillAmount = 25 #33 from excel, 16 for basically covering the minimum (above), 41 for covering the numbers today rounded up for all platforms, actually 45 based on model 39
 
 windmillMaxCapacity = 15*windmillAmount
-MWtoCO2Rate = 0.0625426727765 #how much 1 mw10m co2
+#windmillMaxCapacity = 0
+#windmillMaxCapacity = usedPower
+#bufferCapacity = 0
+MWtoCO2Rate = 0.0651977072657 #how much 1 mw10m co2 0.06254267277645784 0.0625426727765 0.0637079346929
+
 print(MWtoCO2Rate)
-CarbonTax = 590 #590 nok/meteric ton currently
+CarbonTax = 1812.43 #590 nok/meteric ton currently, 632 after update
+defaultCarbon = 766763.81
 
 #comment out below when running with buffers and
 #windmillMaxCapacity = 0 #for pure C02
@@ -28,6 +34,9 @@ CarbonTax = 590 #590 nok/meteric ton currently
 #179876 oseberg sør
 #337380 + 234497 feltsenter
 #228,988 MW
+co2aa = 337380 + 234497 + 179876
+MWs = usedPower*6*24*365 #51359 actual, 52560 calculated
+print(co2aa/MWs, " Megawatts: ", MWs)
 
 
 
@@ -42,8 +51,11 @@ def CalculateForBuffer(differenceToPower, bufferCurrentSize):
         if differenceToPower*bufferFillingEfficiency + bufferCurrentSize > bufferCapacity:
             bufferCurrentSize = bufferCapacity
             writeToArray = bufferCapacity - bufferCurrentSize*bufferFillingEfficiency
-            writeAccessPower = differenceToPower - (bufferCapacity - bufferCurrentSize)/bufferCurrentSize
-            print("ye hallo")
+            if bufferCurrentSize == 0:
+                writeAccessPower = differenceToPower
+            else:
+                writeAccessPower = differenceToPower - (bufferCapacity - bufferCurrentSize)/bufferCurrentSize
+            #print("ye hallo")
         else:
             bufferCurrentSize += differenceToPower*bufferFillingEfficiency
             writeToArray = differenceToPower*bufferFillingEfficiency
@@ -68,18 +80,26 @@ header = next(csvreader)
 print(header)
 windData = []
 dates = []
+actualWindSpeeds = []
+windData25 = []
 df = pd.read_csv('windForOseberg.csv')
 print(df)
 for row in csvreader:
     windData.append(row[1])
     dates.append(row[2])
+    actualWindSpeeds.append(row[3])
+    windData25.append(row[5])
 #print(windData)
 file.close()
 windData = pd.array(windData, dtype= float)
+windData25 = pd.array(windData25, dtype= float)
 dates = pd.array(dates, dtype = 'datetime64')
+actualWindSpeeds = pd.array(actualWindSpeeds, dtype= float)
 print(len(windData))
 windA = windData[0:51359]
 datesA = dates[0:51359]
+windSpeedA = actualWindSpeeds[0:51359]
+wind25A = windData25[0:51359]
 print(numpy.mean(windA)) #averages provide the exact lower value for where the wind provides enough energy for one year
 windC = windData[51359:103366]
 datesC = dates[51359:103366]
@@ -104,6 +124,7 @@ for number in windSor: #longest for windA with all zeroes is a little bit more t
 print("longest zero", longestZero)
 
 windDataUse = windA*windmillMaxCapacity
+windSpeedUse = windSpeedA
 DatesUse = datesA
 powerUsage = numpy.full(len(windDataUse), powerConsumption)
 currentBufferCurrentSize = numpy.full(len(windDataUse), 0, dtype=numpy.double)
@@ -112,42 +133,67 @@ currentBufferusedEnergy = numpy.full(len(windDataUse), 0, dtype=numpy.double)
 excessEnergy = numpy.full(len(windDataUse), 0, dtype=numpy.double)
 extraOutput = numpy.full(len(windDataUse), 0)
 print("buffersize", bufferCurrentSize , "max ", bufferCapacity)
+totalWastedEnergy = numpy.full(len(windDataUse), 0, dtype=numpy.double)
+averageOutput = 0
+counterRefill = 0
+landPowerIn = numpy.full(len(windDataUse), 0, dtype=numpy.double)
+cableLimit = 150
 for i in range(len(windDataUse)):
     bufferCurrentSize, currentBufferPowerOutput[i], excessEnergy[i] = CalculateForBuffer(windDataUse[i]-powerConsumption, bufferCurrentSize)
+
+    if counterRefill >= 6*24*7:
+        #bufferCurrentSize = bufferCapacity
+        counterRefill = 0
+    else:
+        counterRefill = counterRefill + 1
     currentBufferCurrentSize[i] = bufferCurrentSize
+    averageOutput += windDataUse[i]
     if currentBufferPowerOutput[i]<0 :
         currentBufferusedEnergy[i] = -currentBufferPowerOutput[i]
     if windDataUse<=powerConsumption and windDataUse[i] < powerConsumption:
         extraOutput[i] = powerConsumption - windDataUse[i] - currentBufferusedEnergy[i]
+        if extraOutput[i] > cableLimit:
+            extraOutput[i] = extraOutput[i] - cableLimit
+            landPowerIn[i] = cableLimit
+        else:
+            landPowerIn[i] = extraOutput[i]
+            extraOutput[i] = 0
 print(powerUsage)
+averageOutput = averageOutput/len(windDataUse)
+print("average output ", averageOutput)
 fig = go.Figure()
-
+timeUse = len(windDataUse) #6*24*7
+windDataUse = windDataUse[0:timeUse]
+currentBufferusedEnergy = currentBufferusedEnergy[0:timeUse]
+extraOutput = extraOutput[0:timeUse]
+excessEnergy = excessEnergy[0:timeUse]
+DatesUse = DatesUse[0:timeUse]
 fig.add_trace(go.Scatter(x=DatesUse, y=windDataUse, mode='lines',
-        name="Wind",
+        name="Windmill effect output",
         line=dict(color='rgb(0,0,0)', width=2),
         connectgaps=True,
     ))
 
-fig.add_trace(go.Scatter(x=DatesUse, y=powerUsage, mode='lines',
-        name="used_Energy",
-        line=dict(color='rgb(0,255,0)', width=2),
-        connectgaps=True,
-        ))
+#fig.add_trace(go.Scatter(x=DatesUse, y=currentBufferusedEnergy, mode='lines',
+#        name="Buffer effect output",
+#        line=dict(color='rgb(255,0,0)', width=2),
+#        connectgaps=True,
+#        ))
 
-fig.add_trace(go.Scatter(x=DatesUse, y=currentBufferusedEnergy, mode='lines',
-        name="buffer_energy_used",
+fig.add_trace(go.Scatter(x=DatesUse, y=landPowerIn, mode='lines',
+        name="Land Power used",
         line=dict(color='rgb(255,0,0)', width=2),
         connectgaps=True,
         ))
 
 fig.add_trace(go.Scatter(x=DatesUse, y=extraOutput, mode='lines',
-        name="extra energy",
+        name="Gas turbine effect output",
         line=dict(color='rgb(0,0,255)', width=2),
         connectgaps=True,
         ))
 
 fig.add_trace(go.Scatter(x=DatesUse, y=excessEnergy, mode='lines',
-        name="excess energy (wasted)",
+        name="Excess windmill effect (wasted)",
         line=dict(color='rgb(255,0,255)', width=2),
         connectgaps=True,
         ))
@@ -155,22 +201,17 @@ fig.add_trace(go.Scatter(x=DatesUse, y=excessEnergy, mode='lines',
 
 fig.show()
 
-fig2 = go.Figure()
 
-fig2.add_trace(go.Scatter(x=DatesUse, y=currentBufferCurrentSize, mode='lines',
-        name="extra energy",
-        line=dict(color='rgb(0,0,255)', width=2),
-        connectgaps=True,
-        ))
-fig2.show()
 longestCounter = 0
 counter = 0
 lastWasZero = False
 totalEmptyTime = 0
 totalCo2Output = 0
 totalExcess = 0
+
 for i in range(len(windDataUse)):
     totalExcess += excessEnergy[i]
+    totalWastedEnergy[i] = totalExcess
     if currentBufferCurrentSize[i] == 0 and windDataUse[i] < powerConsumption:
         totalEmptyTime = totalEmptyTime + 1
         totalCo2Output = totalCo2Output + (extraOutput[i] * MWtoCO2Rate)
@@ -182,6 +223,26 @@ for i in range(len(windDataUse)):
     else:
         lastWasZero = False
         counter = 0
+
+fig2 = go.Figure()
+
+fig2.add_trace(go.Scatter(x=DatesUse, y=currentBufferCurrentSize, mode='lines',
+        name="Buffer avaliable effect",
+        line=dict(color='rgb(0,0,255)', width=2),
+        connectgaps=True,
+        ))
+#fig2.add_trace(go.Scatter(x=DatesUse, y=totalWastedEnergy, mode='lines',
+#        name="Accumulated effect overproduced (wasted)",
+#        line=dict(color='rgb(0,255,0)', width=2),
+#        connectgaps=True,
+#        ))
+
+fig2.add_trace(go.Scatter(x=DatesUse, y=totalWastedEnergy, mode='lines',
+        name="Energy sent to land",
+        line=dict(color='rgb(0,255,0)', width=2),
+        connectgaps=True,
+        ))
+fig2.show()
 print(longestCounter)
 print("longest time with burning gas in hours:", longestCounter/(6))
 print("longest time with burning in days:", longestCounter/(6*24))
@@ -191,3 +252,7 @@ print("total time with burning ga in days:", totalEmptyTime/(6*24))
 print("total tonns CO2 released:", totalCo2Output)
 print("price of CO2 released", totalCo2Output*CarbonTax, "NOK")
 print("total amount of wasted (excess) energy", totalExcess, "MW10M")
+print("percentage of original C02 released", totalCo2Output*100/defaultCarbon, "%")
+
+MW_size = bufferCapacity/(6)
+print("MWh buffer", MW_size)
